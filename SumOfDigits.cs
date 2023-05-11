b445675d0be2200f8369c9122a98b7ca401dfbf1
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-05-11 09:31:39 +0530

    Add files via upload

---
+++ SumOfDigits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11And12
+{
+    internal class SumOfDigits
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+
+        public void Sum()
+
+        {
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sum += digit;
+                num /= 10;
+            }
+            Console.WriteLine("Sum of numbers" + sum);
+        }
+        
+    }
+}
