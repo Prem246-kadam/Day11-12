645446bf747f8e45bddc34dab6d4adf29f52fd3d
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-05-11 09:32:14 +0530

    Add files via upload

---
+++ Unique.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11And12
+{
+    internal class Unique
+    {
+        public  void Uni()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 5, 6, 7, 7, 2, 8 };
+            
+            HashSet<int> set = new HashSet<int>(arr);
+            
+           foreach(int num in set)
+            {
+                Console.WriteLine("Unique elements are" + num);
+           }
+
+
+        }
+    }
+}
