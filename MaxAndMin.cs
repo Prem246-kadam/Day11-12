3a9e27c837a0fbd04ba0d475d72a4695580b7f19
Prem246-kadam <127394840+Prem246-kadam@users.noreply.github.com>
2023-05-11 09:28:52 +0530

    Add files via upload

---
+++ MaxAndMin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day11And12
+{
+    internal class MaxAndMin
+
+    {
+        public void MaxMin()
+        {
+            int[] arr = { 11, 5, 44, 33, 88, 22, 44, 56, 10 };
+            int max = arr[0];
+            int min = arr[0];
+
+                for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+
+            }
+                Console.WriteLine("Maximun element in array" +max);
+                 Console.WriteLine("Minimum element in array" + min);
+
+        }
+    }
+}
